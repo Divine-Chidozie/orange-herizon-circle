6771ba283b6ead0b5f835072322db1c6e878935c
Divine Chidozie <igwedivine92@gmail.com>
2026-07-24 16:40:54 +0100

    Fix Home import casing

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import SignIn from "../pages/auth/SignIn";
 import SignUp from "../pages/auth/SignUp";
 import AccountCreated from "../pages/auth/AccountCreated";
